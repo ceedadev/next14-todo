43440dcf3afefa1cb6d330f446fafaa600ce79eb
ceedadev <ceedadev@gmail.com>
2023-11-21 20:31:06 +0700

    basic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { db } from "@/db/conn";
+
+export default async function Home() {
+  const tasks = await db.query.task.findMany();
   return (
     <main>
       <h1>Simple Todo App, Made with NextJS 14, TailwindCSS, Drizzle ORM</h1>
+      {tasks.map((task) => (
+        <p>{task.title}</p>
+      ))}
     </main>
   );
 }
