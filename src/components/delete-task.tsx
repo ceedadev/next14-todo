dfad8a178a551b2638fc759c154b541ae8fbc82d
Armanda Arif <ceedadev@gmail.com>
2023-11-22 09:48:25 +0700

    refactoring

--- src/components/delete-task.tsx
+++ src/components/delete-task.tsx
@@ -3,8 +3,12 @@
 import { deleteTaskAction } from "@/app/_actions/task";
 
 export default function DeleteTaskButton({ id }: { id: number }) {
-  function onClick(id: number) {
-    deleteTaskAction(id);
-  }
-  return <button onClick={() => onClick(id)}>Delete</button>;
+  return (
+    <button
+      className="hover:font-semibold"
+      onClick={() => deleteTaskAction(id)}
+    >
+      Delete
+    </button>
+  );
 }
