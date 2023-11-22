3ea98ad781d2afb6114b6c170e30b013f9017d2a
Armanda Arif <ceedadev@gmail.com>
2023-11-22 09:33:53 +0700

    modify schema

---
+++ drizzle/0001_overrated_magma.sql
@@ -0,0 +1,1 @@
+ALTER TABLE tasks ADD `edited_at` text;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,5 +4,8 @@ import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 export const task = sqliteTable("tasks", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   title: text("title"),
-  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+  createdAt: text("created_at").notNull(),
+  editedAt: text("edited_at"),
 });
+
+export type SelectTask = typeof task.$inferSelect;
